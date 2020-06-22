f074cec532f0d6adaf7bd17c69cbb5506327cf3c
youngchrisyang <youngchrisyang@gmail.com>
2020-06-22 02:03:03 -0400

    move parameter setup to command line

--- bert_models/affect_binary_model.py
+++ bert_models/affect_binary_model.py
@@ -50,7 +50,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 n_gpu = torch.cuda.device_count()
 torch.cuda.get_device_name(0)
 
-# READING PARAMETERS
+# READING PARAMETERS FROM COMMAND LINE
 import sys
 
 label_name = sys.argv[1]
